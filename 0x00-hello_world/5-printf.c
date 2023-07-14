7c93b423fa343e97ae6eac63b97044e0781a94d8
TiGedjah <titusgedjah01@gmail.com>
2023-07-13 18:33:09 -0700

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* main is always the entry point*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
