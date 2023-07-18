59d78b84fbcf289aae5c218db855f63c6c478262
TiGedjah <titusgedjah01@gmail.com>
2023-07-17 23:52:32 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:  Display 0 as task completed
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
