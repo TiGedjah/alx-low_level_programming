e6f77f7275d0b64e46ed263f5e0eb662688f9fc2
TiGedjah <titusgedjah01@gmail.com>
2023-07-24 11:40:27 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - prints and updates the value to 98
+ *
+ * @n: is the location in memory
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
