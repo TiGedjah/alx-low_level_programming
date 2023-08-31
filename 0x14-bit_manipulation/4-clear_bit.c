815ccaca68b2fdc36b040daee529a827e43b50be
TiGedjah <titusgedjah01@gmail.com>
2023-08-31 11:32:21 -0700

    1

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: pointer
+ * @index: index position
+ *
+ * Return: 1 if true, -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+	unsigned int x;
+
+	if (index > 64)
+		return (-1);
+	x = index;
+	for (i = 1; x > 0; i *= 2, x--)
+		;
+
+	if ((*n >> index) & 1)
+		*n -= i;
+
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip to get from one number to another.
+ * @n: number a
+ * @m: number b
+ *
+ * Return: number of bits to be flipped
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int a;
+	int c;
+
+	a = n ^ m;
+	c = 0;
+
+	while (a)
+	{
+		c++;
+		a &= (a - 1);
+	}
+
+	return (c);
+}
