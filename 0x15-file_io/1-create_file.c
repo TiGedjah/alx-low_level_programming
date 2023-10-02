7215982a46957d35a3e5bb2ff036b5fc6f2d075d
TiGedjah <titusgedjah01@gmail.com>
2023-10-02 09:48:34 -0700

    as

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int a, b, lenn = 0;
+        int a, b, lenn = 0;
 
-	if (filename == NULL)
-		return (-1);
+        if (filename == NULL)
+                return (-1);
 
-	if (text_content != NULL)
-	{
-		for (lenn = 0; text_content[lenn];)
-			lenn++;
-	}
+        if (text_content != NULL)
+        {
+                for (lenn = 0; text_content[lenn];)
+                        lenn++;
+        }
 
-	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(a, text_content, lenn);
+        a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+        w = write(a, text_content, lenn);
 
-	if (a == -1 || w == -1)
-		return (-1);
+        if (a == -1 || w == -1)
+                return (-1);
 
-	close(a);
+        close(a);
 
-	return (1);
+        return (1);
 }
