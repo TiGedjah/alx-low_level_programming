4edcd0dc05b05978b7dbadcd1d924f118181e7fa
TiGedjah <titusgedjah01@gmail.com>
2023-10-02 09:54:41 -0700

    correction 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,32 +2,32 @@
 #include "main.h"
 
 /**
- * create_file - CHecks the code
- * @filename: the name of the file
- * @text_content: a string to write
- *
- * Return: Always Return
- */
+* create_file - CHecks the code
+* @filename: the name of the file
+* @text_content: a string to write
+*
+* Return: Always Return
+*/
 int create_file(const char *filename, char *text_content)
 {
-        int a, b, lenn = 0;
+	int a, b, lenn = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+	return (-1);
 
-        if (text_content != NULL)
-        {
-                for (lenn = 0; text_content[lenn];)
-                        lenn++;
-        }
+	if (text_content != NULL)
+	{
+	for (lenn = 0; text_content[lenn];)
+	lenn++;
+	}
 
-        a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        b = write(a, text_content, lenn);
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(a, text_content, lenn);
 
-        if (a == -1 || b == -1)
-                return (-1);
+	if (a == -1 || b == -1)
+	return (-1);
 
-        close(a);
+	close(a);
 
-        return (1);
+	return (1);
 }
