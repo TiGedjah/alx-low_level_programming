94140ac90f936798d525f5811c4389a888dc9240
TiGedjah <titusgedjah01@gmail.com>
2023-10-02 09:51:54 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,9 +22,9 @@ int create_file(const char *filename, char *text_content)
         }
 
         a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        w = write(a, text_content, lenn);
+        b = write(a, text_content, lenn);
 
-        if (a == -1 || w == -1)
+        if (a == -1 || b == -1)
                 return (-1);
 
         close(a);
